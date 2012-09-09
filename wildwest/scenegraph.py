6b1d079603d733067336f4dbefc0bfa39940dbda
Daniel Pope <lordmauve@users.noreply.github.com>
2012-09-09 23:38:05 +0100

    New: working ground plane and tracks

--- wildwest/scenegraph.py
+++ wildwest/scenegraph.py
@@ -1,29 +1,85 @@
+import math
 import pyglet
 from pyglet import gl
+from collections import namedtuple
 
-pyglet.resource.path += ['../assets/sprites']
+pyglet.resource.path += [
+    '../assets/sprites',
+    '../assets/textures',
+]
 pyglet.resource.reindex()
+
+
+class Rect(namedtuple('BaseRect', 'l r b t')):
+    """2D rectangle class."""    
+
+    @property
+    def w(self):
+        return self.r - self.l
+
+    @property
+    def h(self):
+        return self.t - self.b
 
 
 class Camera(object):
     """A camera object."""
-    def __init__(self, offset):
+    def __init__(self, offset, screen_w, screen_h):
+        self.near = 1.0
+        self.focus = 400.0  # the plane our 2D scene is mainly on
+        self.far = 10000.0
+        self.screen_w = screen_w
+        self.screen_h = screen_h
         self.offset = offset
 
-    def setup_matrixes(self, screen_w, screen_h):
+    def far_plane(self):
+        """Get the rectangle of the back plane"""
+        scale = self.far / self.focus
+        x, y = self.offset
+        return Rect(
+            x + scale * -0.5 * self.screen_w,
+            x + scale * 0.5 * self.screen_w,
+            y + scale * -0.5 * self.screen_h,
+            y + scale * 0.5 * self.screen_h,
+        )
+
+    def near_plane(self):
+        """Get the rectangle of the near plane"""
+        scale = self.near / self.focus
+        x, y = self.offset
+        return Rect(
+            x + scale * -0.5 * self.screen_w,
+            x + scale * 0.5 * self.screen_w,
+            y + scale * -0.5 * self.screen_h,
+            y + scale * 0.5 * self.screen_h,
+        )
+    
+    def viewport(self):
+        """Get the rectangle of the near plane"""
+        x, y = self.offset
+        return Rect(
+            x + -0.5 * self.screen_w,
+            x + 0.5 * self.screen_w,
+            y + -0.5 * self.screen_h,
+            y + 0.5 * self.screen_h,
+        )
+
+    def setup_matrixes(self):
         x, y = self.offset
         gl.glMatrixMode(gl.GL_PROJECTION)
         gl.glLoadIdentity()
-        near = 1.0
-        far = 10000.0
-        l = -0.5 * screen_w / (10 - near)
+#        fov = math.atan(self.screen_h * 0.5 / self.focus)
+#        aspect = self.screen_w * 1.0 / self.screen_h
+#        gl.gluPerspective(fov, aspect, self.near, self.far)
+        l = -0.5 * self.screen_w / (self.focus - self.near)
         r = -l
-        b = -0.5 * screen_h / (10 - near)
+        b = -0.5 * self.screen_h / (self.focus - self.near)
         t = -b
-        gl.glFrustum(l, r, b, t, near, far)
+        self.np = Rect(l, r, b, t)
+        gl.glFrustum(l, r, b, t, self.near, self.far)
         gl.glMatrixMode(gl.GL_MODELVIEW)
         gl.glLoadIdentity()
-        gl.glTranslatef(-x, -y, -10)
+        gl.glTranslatef(-x, -y, -self.focus)
 
 
 
@@ -56,6 +112,64 @@ class StaticImage(AnimatedNode):
         super(StaticImage, self).__init__(pos, im, z)
 
 
+class GroundPlane(Node):
+    z = -900
+
+    def __init__(self, near_colour, far_colour, y=0):
+        self.near_colour = list(near_colour)
+        self.far_colour = list(far_colour)
+        self.y = y
+
+    def draw(self, camera):
+        far = camera.far_plane()
+        near = camera.near_plane()
+        focus = camera.focus
+        coords = ('v3f', [
+            near.l, self.y, focus - camera.near,
+            near.r, self.y, focus - camera.near,
+            far.r, self.y, focus - camera.far,
+            far.l, self.y, focus - camera.far,
+        ])
+        col = ('c4B', self.near_colour * 2 + self.far_colour * 2)
+        pyglet.graphics.draw(4, gl.GL_QUADS, coords, col)
+
+
+class RailTrack(Node):
+    z = -1
+    def __init__(self, tex, y=0):
+        self.dist = 0
+        self.y = y
+        self.group = pyglet.sprite.SpriteGroup(
+            tex,
+            gl.GL_SRC_ALPHA,
+            gl.GL_ONE_MINUS_SRC_ALPHA
+        )
+
+    def draw(self, camera):
+        self.dist += 11.7
+        vp = camera.viewport()
+
+        l = vp.l - 128
+        r = vp.r + 128
+        scale = vp.w / 128
+        coords = ('v3f', [
+            l, self.y, 32,
+            r, self.y, 32,
+            r, self.y, -96,
+            l, self.y, -96,
+        ])
+        d = self.dist / 128.0
+        tc = ('t2f', [
+            1, d,
+            1, d + scale,
+            0, d + scale,
+            0, d
+        ])
+        self.group.set_state()
+        pyglet.graphics.draw(4, gl.GL_QUADS, coords, tc)
+        self.group.unset_state()
+
+
 class Fill(Node):
     z = -1000
 
@@ -68,10 +182,8 @@ class Fill(Node):
 
 
 class Scenegraph(object):
-    def __init__(self, screen_w, screen_h):
+    def __init__(self):
         self.objects = []
-        self.screen_w = screen_w
-        self.screen_h = screen_h
 
     def add(self, obj):
         obj.scenegraph = self
@@ -81,10 +193,7 @@ class Scenegraph(object):
         self.objects.remove(obj)
 
     def draw(self, camera):
-        camera.setup_matrixes(
-            self.screen_w,
-            self.screen_h
-        )
+        camera.setup_matrixes()
         self.objects.sort(key=lambda x: x.z)
         for o in self.objects:
             o.draw(camera)
@@ -94,15 +203,21 @@ if __name__ == '__main__':
     WIDTH = 800
     HEIGHT = 600
     w = pyglet.window.Window(width=800, height=600)
-    s = Scenegraph(WIDTH, HEIGHT)
+    s = Scenegraph()
     s.add(Fill((1.0, 1.0, 1.0, 1.0)))
-    s.add(StaticImage((10, 70), 'car-interior.png'))
-    s.add(StaticImage((90, 167), 'pc-standing.png'))
-    s.add(StaticImage((600, 167), 'lawman-standing.png'))
-    s.add(StaticImage((300, 222), 'table.png'))
-    s.add(StaticImage((500, 197), 'crate.png'))
+    s.add(StaticImage((10, 0), 'car-interior.png'))
+    s.add(StaticImage((90, 115), 'pc-standing.png'))
+    s.add(StaticImage((600, 115), 'lawman-standing.png'))
+    s.add(StaticImage((300, 115), 'table.png'))
+    s.add(StaticImage((500, 115), 'crate.png'))
+    s.add(RailTrack(pyglet.resource.texture('track.png')))
+    ground = GroundPlane(
+        (218, 176, 127, 255),
+        (194, 183, 164, 255),
+    )
+    s.add(ground)
 
-    camera = Camera((400.0, 300.0))
+    camera = Camera((200.0, 200.0), WIDTH, HEIGHT)
 
     @w.event
     def on_draw():
