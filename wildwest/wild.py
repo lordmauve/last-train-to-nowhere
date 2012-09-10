d16e92d6df2a2a2d91eb253de7217a9555b436e2
Arnav Khare <arnav.khare@nokia.com>
2012-09-10 22:06:50 +0100

    Integrate pygame code into pyglet scenegraph

--- wildwest/wild.py
+++ wildwest/wild.py
@@ -1,51 +1,49 @@
-import os
 import sys
 
-import pygame
-
 sys.path.insert(0, '..')
-import retrogamelib
-from retrogamelib.gameobject import Object
-from retrogamelib import display
-from retrogamelib import button
-from retrogamelib.constants import *
-from retrogamelib.camera import Camera
-from retrogamelib import util
-from retrogamelib import geometry
+# import retrogamelib
+# from retrogamelib.constants import *
+# from retrogamelib import geometry
 
-from pygame.time import Clock
+import pyglet
+from pyglet.window import key
+from scenegraph import StaticImage, Scenegraph, Fill, RailTrack
+from scenegraph import SkyBox, GroundPlane, Wheels
+from scenegraph import Camera
+from vector import Vector
 
+# Image File Paths
 ASSETS_BASE = 'assets/sprites'
-IMG_PC_STANDING = os.path.join(ASSETS_BASE, 'pc-standing.png')
-IMG_PC_CROUCHING = os.path.join(ASSETS_BASE, 'pc-crouching.png')
-IMG_LAWMAN_STANDING = os.path.join(ASSETS_BASE, 'lawman-standing.png')
-IMG_LAWMAN_CROUCHING = os.path.join(ASSETS_BASE, 'lawman-crouching.png')
-IMG_TABLE = os.path.join(ASSETS_BASE, 'table.png')
-IMG_CRATE = os.path.join(ASSETS_BASE, 'crate.png')
-IMG_CARRIAGE = os.path.join(ASSETS_BASE, 'car-interior.png')
+IMG_PC_STANDING = 'pc-standing.png'
+IMG_PC_CROUCHING = 'pc-crouching.png'
+IMG_LAWMAN_STANDING = 'lawman-standing.png'
+IMG_LAWMAN_CROUCHING = 'lawman-crouching.png'
+IMG_TABLE = 'table.png'
+IMG_CRATE = 'crate.png'
+IMG_CARRIAGE = 'car-interior.png'
+
+# Key Bindings
+KEY_RIGHT = key.RIGHT
+KEY_LEFT = key.LEFT
+KEY_JUMP = key.Z
+KEY_SHOOT = key.X
 
 FLOOR_Y = 275
 
 
-class Bullet(Object):
+class Bullet(StaticImage):
     pass
 
 
-class ImageObject(Object):
-    def __init__(self, pos, img_path):
-        Object.__init__(self, self.groups)
-        self.image = util.load_image(img_path)
-        self.rect = self.image.get_rect(topleft=pos)
-
-
-class Player(ImageObject):
+class Player(StaticImage):
     MAX_WALK = 200  # limit on walk speed
     ACCEL = 1600  # acceleration when walking
-    FRICTION = 4  # deceleration
+    FRICTION = 1  # deceleration
 
     def __init__(self, pos, img_path):
-        ImageObject.__init__(self, pos, img_path)
-        self.direction = geometry.Vector(1, 0)
+        StaticImage.__init__(self, pos, img_path)
+        self.direction = Vector((1, 0))
+        self.t = 0
         self.walk_speed = 0
         self.xaccel = 0
         self.jump_speed = 10
@@ -53,12 +51,13 @@ class Player(ImageObject):
         self.on_floor = False
         # self.fall_through = 0  # frames of fall_through
 
-    def controls(self):
-        if button.is_pressed(A_BUTTON):
+    def on_key_press(self, symbol, modifiers):
+        if symbol == KEY_JUMP:
             self.jump()
-        if button.is_pressed(B_BUTTON):
+        if symbol == KEY_SHOOT:
             self.shoot()
-        self.control_walk()
+        if symbol in (KEY_LEFT, KEY_RIGHT):
+            self.walk(symbol)
         # self.aim_shot()
         # self.choose_images()
 
@@ -76,17 +75,11 @@ class Player(ImageObject):
         #         self.jump_speed = -11
         #     print 'jump_speed:', self.jump_speed
 
-    def control_walk(self):
-        if button.is_held(LEFT):
+    def walk(self, symbol):
+        if symbol == key.LEFT:
             self.xaccel = -self.ACCEL
-        elif button.is_held(RIGHT):
+        elif symbol == key.RIGHT:
             self.xaccel = self.ACCEL
-        else:
-            if abs(self.walk_speed) < 0.01:
-                self.walk_speed = 0
-                self.xaccel = 0
-            else:
-                self.xaccel = -self.FRICTION * self.walk_speed
 
     # def aim_shot(self):
     #     self.shot_vector = self.direction.copy()
@@ -98,32 +91,47 @@ class Player(ImageObject):
     #         self.shot_vector.x = x
     #     self.shot_vector.y = y
 
-    def do_walk(self):
-        if self.walk_speed:
-            self.rect.x = int(self.rect.x + self.walk_speed)
-            self.direction.x = self.walk_speed / abs(self.walk_speed)
+    # def do_walk(self):
+    #     if self.walk_speed:
+    #         self.rect.x = int(self.rect.x + self.walk_speed)
+    #         self.direction.x = self.walk_speed / abs(self.walk_speed)
 
-    def update(self, dt):
+    def draw(self, camera):
+        dt = self.scenegraph.t - self.t
+        self.t = self.scenegraph.t
+
+        # update horizontally
         u = self.walk_speed
         self.walk_speed += self.xaccel * dt
         if abs(self.walk_speed) > self.MAX_WALK:
             self.walk_speed = self.MAX_WALK * self.walk_speed / abs(self.walk_speed)
-        self.rect.x += 0.5 * (u + self.walk_speed) * dt
-        # if self.jump_speed < 7:
-        #     self.jump_speed += 1
-        # if self.jump_speed < 10 and\
-        if (self.jumping or self.rect.y < FLOOR_Y - self.rect.height):
-            self.jump_speed += 1
-            self.rect.y += self.jump_speed
-            if self.rect.y > FLOOR_Y - self.rect.height:
-                self.rect.y = FLOOR_Y - self.rect.height
-            #print 'jump_speed: %d, rect.y: %d' % (self.jump_speed, self.rect.y)
+        self.sprite.x += 0.5 * (u + self.walk_speed) * dt
 
-        #self.do_walk()
-        self.on_floor = False
-        self.collide_with_floors()
-        # if self.fall_through > 0:
-        #     self.fall_through -= 1
+        super(Player, self).draw(camera)
+
+        # update acceleration
+        if abs(self.walk_speed) < 0.01:
+            self.walk_speed = 0
+            self.xaccel = 0
+        else:
+            self.xaccel = -self.FRICTION * self.walk_speed
+
+        # update vertically
+        # # if self.jump_speed < 7:
+        # #     self.jump_speed += 1
+        # # if self.jump_speed < 10 and\
+        # if (self.jumping or self.sprite.y < FLOOR_Y - self.sprite.height):
+        #     self.jump_speed += 1
+        #     self.sprite.y += self.jump_speed
+        #     if self.sprite.y > FLOOR_Y - self.sprite.height:
+        #         self.sprite.y = FLOOR_Y - self.sprite.height
+        #     #print 'jump_speed: %d, rect.y: %d' % (self.jump_speed, self.rect.y)
+
+        # #self.do_walk()
+        # self.on_floor = False
+        # self.collide_with_floors()
+        # # if self.fall_through > 0:
+        # #     self.fall_through -= 1
 
     def collide_with_floors(self):
         # print list(self.platforms)
@@ -147,14 +155,14 @@ class Player(ImageObject):
         self.jumping = False
 
 
-class Table(ImageObject):
+class Table(StaticImage):
     def __init__(self, pos):
-        ImageObject.__init__(self, pos, IMG_TABLE)
+        StaticImage.__init__(self, pos, IMG_TABLE)
 
 
-class Crate(ImageObject):
+class Crate(StaticImage):
     def __init__(self, pos):
-        ImageObject.__init__(self, pos, IMG_CRATE)
+        StaticImage.__init__(self, pos, IMG_CRATE)
 
 
 class Hero(Player):
@@ -162,70 +170,60 @@ class Hero(Player):
         Player.__init__(self, pos, IMG_PC_STANDING)
 
 
-class Lawman(Player):
+class Lawman(StaticImage):
     def __init__(self, pos):
-        Player.__init__(self, pos, IMG_LAWMAN_STANDING)
+        StaticImage.__init__(self, pos, IMG_LAWMAN_STANDING)
 
 
-class Carriage(Object):
+class Carriage(StaticImage):
     def __init__(self, pos):
-        Object.__init__(self, self.groups)
-        image = util.load_image(IMG_CARRIAGE)
-        self.image = pygame.transform.scale(image, (1024, 319))
-        self.rect = self.image.get_rect(topleft=pos)
+        StaticImage.__init__(self, pos, IMG_CARRIAGE)
         self.allow_fall_through = False
 
 
 def main():
-    display.init(1.0, 'Train to Nowhere', res=(1044, 400))
+    WIDTH = 800
+    HEIGHT = 600
+    w = pyglet.window.Window(width=800, height=600)
+    s = Scenegraph()
 
-    objects = retrogamelib.gameobject.Group()
-    platforms = retrogamelib.gameobject.Group()
-    players = retrogamelib.gameobject.Group()
-    # bullets = retrogamelib.gameobject.Group()
+    s.add(Fill((1.0, 1.0, 1.0, 1.0)))
+    s.add(Carriage((0, 53)))
+    s.add(Hero((90, 115)))
+    s.add(Lawman((600, 115)))
+    s.add(Table((300, 115)))
+    s.add(Crate((500, 115)))
+    s.add(RailTrack(pyglet.resource.texture('track.png')))
+    s.add(Wheels((91, 0)))
+    s.add(Wheels((992 - 236, 0)))
 
-    Carriage.groups = [platforms, objects]
-    Player.groups = [players, objects]
-    Table.groups = [objects]
-    Crate.groups = [objects]
-    Player.platforms = platforms
+    ground = GroundPlane(
+        (218, 176, 127, 255),
+        (194, 183, 164, 255),
+    )
+    s.add(ground)
 
-    Carriage((10, 70))
-    hero = Hero((90, 167))
-    # Lawman((600, 165))
-    Table((300, 222))
-    Crate((500, 197))
+    s.add(SkyBox(
+        (129, 218, 255, 255),
+        (49, 92, 142, 255)
+    ))
 
-    camera = Camera()
-    camera.follow(hero)
+    camera = Camera((200.0, 200.0), WIDTH, HEIGHT)
 
-    clock = Clock()
+    @w.event
+    def on_draw():
+        s.draw(camera)
 
-    while True:
-        dt = clock.tick(60)
-        dt *= 0.001
-        button.handle_input()
-        hero.controls()
-        hero.update(dt)
-        for o in objects:
-            if o is not hero:
-                o.update()
-        camera.update()
+    @w.event
+    def on_key_press(symbol, modifiers):
+        s.on_key_press(symbol, modifiers)
 
-        surface = display.surface
-        surface.fill((0, 0, 180))
+    def update(dt):
+        s.update(dt)
 
-        for object in objects:
-            translated = camera.translate(object.rect)
-            surface.blit(object.image, translated)
-            if translated.x < -16 or translated.x > 300:
-                if hasattr(object, 'offscreen'):
-                    object.offscreen(camera)
-            else:
-                if hasattr(object, 'onscreen'):
-                    object.onscreen(camera)
+    pyglet.clock.schedule_interval(update, 1 / 30.0)
+    pyglet.app.run()
 
-        display.update()
 
 if __name__ == '__main__':
     main()
